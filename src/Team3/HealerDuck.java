aad5723234d7038a4acf2c4c26d0f2a5e0974ec7
Sanad Thapa <sanadtm@gmail.com>
2024-10-19 23:19:17 -0700

    Added Movement for Healer Duck

--- src/Team3/HealerDuck.java
+++ src/Team3/HealerDuck.java
@@ -3,6 +3,7 @@ package Team3;
 import battlecode.common.*;
 
 public class HealerDuck extends Duck {
+    static Direction direction;
     public HealerDuck(RobotController rc) {
         super(rc);
         skill = SkillType.HEAL;
@@ -13,5 +14,45 @@ public class HealerDuck extends Duck {
     }
 
     public void heal() throws GameActionException {}
-    public void move() throws GameActionException {}
+    public void move() throws GameActionException {
+        MapLocation[] locations = rc.getAllySpawnLocations();
+        if (rc.hasFlag()) {
+            // move toward ally spawn locations
+            // TODO: don't move blindly toward locations[0]
+            moveToward(locations[0]);
+        } else {
+            // move toward adversary spawn locations
+            // TODO: don't move blindly away from locations[0]
+            moveAwayFrom(locations[0]);
+        }
+    }
+
+    @Override public void moveToward(MapLocation location) throws GameActionException {
+        Direction direction = rc.getLocation().directionTo(location);
+        if (rc.canMove(direction)) {
+            rc.move(direction);
+        } else {
+            for (Direction otherDirection : Direction.allDirections()) {
+                if (rc.canMove(otherDirection)) {
+                    rc.move(otherDirection);
+                    break;
+                }
+            }
+        }
+    }
+    public static void lookForCrumb(RobotController rc) throws GameActionException {
+        MapLocation [] closeByCrumbs = rc.senseNearbyCrumbs(-1);
+        if(closeByCrumbs != null && closeByCrumbs.length > 0) {
+            Direction crumbDir = rc.getLocation().directionTo(closeByCrumbs[0]);
+            if(rc.canMove(crumbDir)) rc.move(crumbDir);
+        }
+        if(rc.isMovementReady()){
+            if(rc.isMovementReady()){
+                if(direction !=null &&rc.canMove(direction))rc.move(direction);
+            }
+            else{
+                direction = Direction.allDirections()[RobotPlayer.rng.nextInt(Direction.allDirections().length)];
+            }
+        }
+    }
 }
