9b983106f0cbf3a0470fe64a9aad36b7e3f44e80
Sanad Thapa <sanadtm@gmail.com>
2024-10-20 13:03:48 -0700

    Added Navigation Code to Healre Duck

--- src/Team3/HealerDuck.java
+++ src/Team3/HealerDuck.java
@@ -4,34 +4,62 @@ import battlecode.common.*;
 
 public class HealerDuck extends Duck {
     static Direction direction;
-    public HealerDuck(RobotController rc) {
+    public HealerDuck(RobotController rc)  throws GameActionException {
         super(rc);
         skill = SkillType.HEAL;
+        play();
     }
 
     @Override public void play() throws GameActionException {
         super.play();
+        lookForFlag(rc);
+        exploreAround(rc);
+        move();
+    }
+    public static void lookForFlag(RobotController rc) throws GameActionException {
+        FlagInfo[]flags = rc.senseNearbyFlags(-1, rc.getTeam());
+        for(FlagInfo flag : flags){
+            if(rc.canPickupFlag(flag.getLocation())){
+                rc.canPickupFlag(flag.getLocation());
+                break;
+            }
+        }
     }
 
+    private static void moveTowardAllySpawnZone(RobotController rc) throws GameActionException {
+        MapLocation[] spawnLocs = rc.getAllySpawnLocations();
+        MapLocation firstLoc = spawnLocs[0];
+        Direction dir = rc.getLocation().directionTo(firstLoc);
+        if (rc.canMove(dir)) rc.move(dir);
+    }
     public void heal() throws GameActionException {}
     public void move() throws GameActionException {
         MapLocation[] locations = rc.getAllySpawnLocations();
         if (rc.hasFlag()) {
-            // move toward ally spawn locations
-            // TODO: don't move blindly toward locations[0]
-            moveToward(locations[0]);
+            moveTowardAllySpawnZone(rc);
         } else {
             // move toward adversary spawn locations
             // TODO: don't move blindly away from locations[0]
-            moveAwayFrom(locations[0]);
+            lookForFlag(rc); // Look For Flag
         }
     }
-
-    @Override public void moveToward(MapLocation location) throws GameActionException {
-        Direction direction = rc.getLocation().directionTo(location);
-        if (rc.canMove(direction)) {
-            rc.move(direction);
-        } else {
+    private static void moveTowardEnemySpawnZone(RobotController rc) throws GameActionException {
+        MapLocation[] spawnLocs = rc.getAllySpawnLocations();
+        MapLocation firstLoc = spawnLocs[0];
+        Direction dir = rc.getLocation().directionTo(firstLoc);
+        if (rc.canMove(dir)) rc.move(dir);
+    }
+     public void moveToward(RobotController rc,MapLocation location) throws GameActionException {
+     direction = rc.getLocation().directionTo(location);
+     if(rc.hasFlag()){
+       moveTowardAllySpawnZone(rc);
+     }
+     else if (rc.canMove(direction)) {
+       rc.move(direction);
+     }
+     else if (rc.canFill(rc.getLocation().add(direction))) rc.fill(rc.getLocation().add(direction));
+     else {
+        // Direction randDirc = Direction.allDirections()[RobotPlayer.rng.nextInt(Direction.allDirections().length)];
             for (Direction otherDirection : Direction.allDirections()) {
                 if (rc.canMove(otherDirection)) {
                     rc.move(otherDirection);
@@ -40,7 +68,7 @@ public class HealerDuck extends Duck {
             }
         }
     }
-    public static void lookForCrumb(RobotController rc) throws GameActionException {
+    public static void exploreAround(RobotController rc) throws GameActionException {
         MapLocation [] closeByCrumbs = rc.senseNearbyCrumbs(-1);
         if(closeByCrumbs != null && closeByCrumbs.length > 0) {
             Direction crumbDir = rc.getLocation().directionTo(closeByCrumbs[0]);
