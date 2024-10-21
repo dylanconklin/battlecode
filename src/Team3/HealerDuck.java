18637b6b3bf8322cd9a944bea3a77eb505487df5
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-10-20 23:11:26 -0700

    Removed Error That Is Never Thrown

--- src/Team3/HealerDuck.java
+++ src/Team3/HealerDuck.java
@@ -3,7 +3,7 @@ package Team3;
 import battlecode.common.*;
 
 public class HealerDuck extends Duck {
-    public HealerDuck(RobotController rc) throws GameActionException {
+    public HealerDuck(RobotController rc) {
         super(rc);
         skill = SkillType.HEAL;
     }
