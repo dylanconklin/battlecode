18157a2eac6666cc98ae3d97f4d00de274dea17b
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-10-22 17:46:27 -0700

    Removed Unnecessary Checks

--- src/Team3/AttackerDuck.java
+++ src/Team3/AttackerDuck.java
@@ -19,22 +19,23 @@ public class AttackerDuck extends Duck {
         RobotInfo[] robotInfos = rc.senseNearbyRobots();
         Team rcTeam = rc.getTeam();
         for (RobotInfo robot : robotInfos) {
-            if (robot.team != rcTeam && rc.canAttack(robot.location)) {
+             if (rc.canAttack(robot.location)) {
                 rc.attack(robot.location);
-            }
+             }
         }
     }
 
     public void move() throws GameActionException {
-        while (rc.getRoundNum() >= GameConstants.SETUP_ROUNDS && (rc.hasFlag() || rc.getHealAmount() <= 300)) {
+        if (rc.getRoundNum() >= GameConstants.SETUP_ROUNDS && (rc.hasFlag() || rc.getHealAmount() <= 300)) {
             // move toward ally spawn locations
             // TODO: don't move blindly toward locations[0]
             moveToward(allySpawnZoneDirection());
-        }
-        if (rc.getRoundNum() >= GameConstants.SETUP_ROUNDS) {
+        } else if (rc.getRoundNum() >= GameConstants.SETUP_ROUNDS) {
             // move toward adversary spawn locations
             // TODO: don't move blindly away from locations[0]
             moveToward(enemySpawnZoneDirection());
+        } else {
+            moveToward(allySpawnZoneDirection());
         }
     }
 }
