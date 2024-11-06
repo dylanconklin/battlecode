6a57a383f9e40bf6581f0fb1b2583d1f7d391a07
nila <nila@pdx.edu>
2024-11-05 16:55:24 -0800

    debugging the healer duck move.

--- src/Team3/HealerDuck.java
+++ src/Team3/HealerDuck.java
@@ -3,16 +3,16 @@ package Team3;
 import battlecode.common.*;
 public class HealerDuck extends Duck {
 
-    private static final int MAX_HEALTH_THRESHOLD = 300;
+    //total health is 1000 and healing to be done when life drops below 900.
+    private static final int MAX_HEALTH_THRESHOLD = 900;
 
     public HealerDuck(RobotController rc) {
         super(rc);
         skill = SkillType.HEAL;
-        System.out.println("DBG: HealDuck");
+
     }
 
     // this method will return true / false based on the fact if it is healing or not. this return can be utilized
-    // to take a move action upon not healing.
 
     public void exploreAround() throws GameActionException {
         MapLocation[] closeByCrumbs = rc.senseNearbyCrumbs(-1);
@@ -27,23 +27,20 @@ public class HealerDuck extends Duck {
         MapLocation ml = rc.getLocation();
         if (ml == null)
         {
+            // need to debug the issue with move why the location is null sometime. 
             System.out.println("DBG: H: ml is null");
             return;
         }
-        heal();
-        lookForFlag();
-        move();
-        /*
-        if (!heal()) {  // Try to heal first, and only proceed if no healing was done
+
+        if (!heal_ally()) {  // Try to heal first, and only proceed if no healing was done
             lookForFlag();
             exploreAround();
             move();
         }
 
-         */
     }
 
-    private boolean heal() throws GameActionException {
+    private boolean heal_ally() throws GameActionException {
         // heal () should be called from move method.
 
         // sensing all the robots near in its vision to heal. it will heal only the ally robots.
@@ -57,20 +54,35 @@ public class HealerDuck extends Duck {
         if (nearbyAllies != null)
         {
             for (RobotInfo ally : nearbyAllies) {
-                //System.out.println("DBG: Heal stats" +ally.getHealth());
-                // need to find the constants and replace 100 with that HP constants (better not to use hardcode value)
-                if (ally.getHealth() <= MAX_HEALTH_THRESHOLD) {
-                    System.out.println("DBG: Heal needs" + ally.location + "can heal? " + rc.canHeal(ally.location));
+                int b_heal_lvl = ally.getHealLevel();
+                int b_health_lvl = ally.getHealth();
+                if(ally.getHealLevel() >0 ||ally.getHealth()< 1000) {
+                   // System.out.println("DBG: Heal stats" + ally.getHealLevel() + "health: " + ally.getHealth());
+                }
+
+                // healing should be enabled when health is less than 1000.
+                if (ally.getHealth() < MAX_HEALTH_THRESHOLD) {
+
                     // Heal the ally if it's within healing range
+
                     if (rc.canHeal(ally.location)) {
+
                         rc.heal(ally.location);
-                        System.out.println("DBG: Heal done");
+
                         // add experience while healing.
                         rc.getExperience(skill);
+
                         didHeal = true;
-                        break;
+                        int a_heal_lvl = ally.getHealLevel();
+                        int a_health_lvl = ally.getHealth();
+                        if(a_health_lvl!= b_health_lvl) {
+                            System.out.println("DBG: Heal stats after" + ally.getHealLevel() + "health: " + ally.getHealth());
+                        }
+
+                        //break;
                     }
                 }
+
             }
         }
 
@@ -78,25 +90,15 @@ public class HealerDuck extends Duck {
     }
 
     public void move() throws GameActionException {
-        if (rc.getRoundNum() >= GameConstants.SETUP_ROUNDS ) {
-            // move toward ally spawn locations
-            // TODO: don't move blindly toward locations[0]
-            moveToward(allySpawnZoneDirection());
-        } else if (rc.getRoundNum() >= GameConstants.SETUP_ROUNDS) {
-            // move toward adversary spawn locations
-            // TODO: don't move blindly away from locations[0]
+
+        if (rc.getRoundNum() <= GameConstants.SETUP_ROUNDS ){
             moveToward(enemySpawnZoneDirection());
-        } else {
-            moveInRandomDirection();
         }
-
-        /*
-        if (rc.hasFlag()) {
+        else if (rc.hasFlag()) {
             moveToward(allySpawnZoneDirection());
         } else {
             moveInRandomDirection();
         }
 
-         */
     }
 }
