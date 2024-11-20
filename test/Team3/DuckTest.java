1409c99e9b68d8beb2c6141a8f16c8b354ac174b
Sanad Thapa <sanadtm@gmail.com>
2024-11-19 22:19:45 -0800

    Added Test Cases for Duck Base Class which is testUpdateEnemyRobots

--- test/Team3/DuckTest.java
+++ test/Team3/DuckTest.java
@@ -19,4 +19,26 @@ class DuckTest {
         rc = mock(RobotController.class);
         duck = new Duck(rc);
     }
+
+    @Test
+    void testUpdateEnemyRobots() throws GameActionException {
+        Team myTeam = Team.A;
+        Team opponentTeam = Team.B;
+
+        // Mock team retrieval
+        when(rc.getTeam()).thenReturn(myTeam);
+
+        RobotInfo[] enemyRobots = {
+                new RobotInfo(1, Team.A, 70, new MapLocation(1, 12), false, 1, 1, 1),
+                new RobotInfo(2, Team.A, 100, new MapLocation(1, 123), false, 1, 1, 1)
+        };
+        when(rc.senseNearbyRobots(-1, Team.B)).thenReturn(enemyRobots);
+        when(rc.canWriteSharedArray(0, enemyRobots.length)).thenReturn(true);
+        when(rc.senseNearbyRobots(-1, Team.B)).thenReturn(enemyRobots);
+
+
+        duck.updateEnemyRobots();
+        verify(rc, times(1)).writeSharedArray(0, enemyRobots.length);
+    }
+
 }
