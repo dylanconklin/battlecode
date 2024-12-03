521dc991dc3134f18028803b57bbdcb7baf174df
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-12-03 11:36:15 -0800

    Bring BuilderDuck Up to Standard

--- src/Team3/BuilderDuck.java
+++ src/Team3/BuilderDuck.java
@@ -5,14 +5,29 @@ import battlecode.common.*;
 import java.util.ArrayList;
 
 /**
- * BuilderDuck class to defend flags, place traps, adapt terrain, and maintain resources.
+ * BuilderDuck class to defend flags, place traps,
+ * adapt terrain, and maintain resources.
  */
 public class BuilderDuck extends Duck {
+    /**
+     * Threshold.
+     */
+    public static final int FOUR = 4;
 
-    public BuilderDuck(RobotController rc) {
+    /**
+     * Constructor for BuilderDuck.
+     *
+     * @param rc
+     */
+    public BuilderDuck(final RobotController rc) {
         super(rc, SkillType.BUILD);
     }
 
+    /**
+     * Game logic for BuilderDuck.
+     *
+     * @throws GameActionException
+     */
     @Override
     public void play() throws GameActionException {
         super.setupPlay();
@@ -35,13 +50,15 @@ public class BuilderDuck extends Duck {
 
     /**
      * Defend the flag by staying near it and positioning traps.
+     *
      * @throws GameActionException
      */
     public void defendFlag() throws GameActionException {
         RobotController rc = getRobotController();
-        FlagInfo[] flags = rc.senseNearbyFlags(GameConstants.VISION_RADIUS_SQUARED, rc.getTeam());
+        FlagInfo[] flags = rc.senseNearbyFlags(
+                GameConstants.VISION_RADIUS_SQUARED, rc.getTeam());
         for (FlagInfo flag : flags) {
-            if (rc.getLocation().distanceSquaredTo(flag.getLocation()) > 4) {
+            if (rc.getLocation().distanceSquaredTo(flag.getLocation()) > FOUR) {
                 moveToward(flag.getLocation());
                 break;
             }
@@ -50,6 +67,7 @@ public class BuilderDuck extends Duck {
 
     /**
      * Place traps to defend flags or key locations.
+     *
      * @throws GameActionException
      */
     public void placeTraps() throws GameActionException {
@@ -76,6 +94,7 @@ public class BuilderDuck extends Duck {
 
     /**
      * Handles water obstacles by filling tiles.
+     *
      * @throws GameActionException
      */
     public void handleWaterObstacles() throws GameActionException {
@@ -93,13 +112,15 @@ public class BuilderDuck extends Duck {
 
     /**
      * Collect crumbs to maintain a supply for building and defending.
+     *
      * @return True if Duck moved, False otherwise
      * @throws GameActionException
      */
     @Override
     public boolean collectCrumbs() throws GameActionException {
         RobotController rc = getRobotController();
-        MapLocation[] crumbLocations = rc.senseNearbyCrumbs(GameConstants.VISION_RADIUS_SQUARED);
+        MapLocation[] crumbLocations = rc.senseNearbyCrumbs(
+                GameConstants.VISION_RADIUS_SQUARED);
         boolean didMove = false;
         if (crumbLocations.length > 0) {
             didMove = moveToward(crumbLocations[0]);
@@ -111,6 +132,7 @@ public class BuilderDuck extends Duck {
 
     /**
      * Move towards important areas or explore randomly.
+     *
      * @throws GameActionException
      */
     public void move() throws GameActionException {
@@ -118,7 +140,8 @@ public class BuilderDuck extends Duck {
 
         // If no immediate tasks are required, move randomly
         if (!moveInRandomDirection()) {
-            Direction randomDirection = RobotPlayer.DIRECTIONS[RobotPlayer.RNG.nextInt(RobotPlayer.DIRECTIONS.length)];
+            Direction randomDirection = RobotPlayer.DIRECTIONS[
+                    RobotPlayer.RNG.nextInt(RobotPlayer.DIRECTIONS.length)];
             if (rc.canMove(randomDirection)) {
                 rc.move(randomDirection);
             }
