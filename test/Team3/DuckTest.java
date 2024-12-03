689064f985be84218993a64e1cbbf3265caecbec
Sanad Thapa <sanadtm@gmail.com>
2024-12-03 15:23:14 -0800

    Added test cases for duck test

--- test/Team3/DuckTest.java
+++ test/Team3/DuckTest.java
@@ -19,4 +19,73 @@ class DuckTest {
         rc = mock(RobotController.class);
         duck = new Duck(rc, SkillType.ATTACK);
     }
+    @Test
+    void testUpdateEnemyRobots() throws GameActionException {
+        Team myTeam = Team.A;
+        Team opponentTeam = Team.B;
+        when(rc.getTeam()).thenReturn(myTeam);
+        RobotInfo[] enemyRobots = {
+                new RobotInfo(1, Team.A, 70, new MapLocation(1, 12), false, 1, 1, 1),
+                new RobotInfo(2, Team.B, 100, new MapLocation(1, 123), false, 1, 1, 1)
+        };
+
+        when(rc.senseNearbyRobots(-1, opponentTeam)).thenReturn(enemyRobots);
+        when(rc.canWriteSharedArray(anyInt(), anyInt())).thenReturn(true);
+
+        duck.updateEnemyRobots();
+
+        verify(rc, times(1)).writeSharedArray(anyInt(), anyInt());
+    }
+
+    @Test
+    void testLookForFlag() throws GameActionException {
+        FlagInfo flag = new FlagInfo(new MapLocation(3, 3), Team.A, true, 12);
+        FlagInfo[] flags = { flag };
+
+        when(rc.senseNearbyFlags(-1, rc.getTeam())).thenReturn(flags);
+        when(rc.canPickupFlag(flag.getLocation())).thenReturn(true);
+
+        boolean pickedUpFlag = duck.lookForFlag();
+
+        assertTrue(pickedUpFlag);
+        verify(rc, times(1)).pickupFlag(flag.getLocation());
+    }
+
+    @Test
+    void testMoveAwayFromLocation() throws GameActionException {
+        MapLocation enemyLocation = new MapLocation(10, 10);
+        when(rc.getLocation()).thenReturn(new MapLocation(5, 5));
+        when(rc.canMove(any(Direction.class))).thenReturn(true);
+
+        boolean result = duck.moveAwayFrom(enemyLocation);
+
+        assertTrue(result, "Duck should move away from the enemy location");
+        verify(rc, atLeastOnce()).move(any(Direction.class));
+    }
+
+    @Test
+    void testMoveToward() throws GameActionException {
+        MapLocation currentLocation = new MapLocation(0, 0);
+        Direction direction = Direction.NORTH;
+        MapLocation targetLocation = currentLocation.add(direction);
+
+        when(rc.getLocation()).thenReturn(currentLocation);
+        when(rc.canMove(direction)).thenReturn(true);
+
+        boolean moved = duck.moveToward(direction);
+
+        assertTrue(moved, "Duck should move toward the specified direction");
+        verify(rc, times(1)).move(direction);
+    }
+
+    @Test
+    void testMoveInRandomDirection() throws GameActionException {
+        when(rc.canMove(any(Direction.class))).thenReturn(true);
+
+        boolean moved = duck.moveInRandomDirection();
+
+        assertTrue(moved, "Duck should move in a random direction");
+        verify(rc, atLeastOnce()).move(any(Direction.class));
+    }
+
 }
