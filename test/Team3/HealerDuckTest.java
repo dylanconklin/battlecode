e25f982ecf9f5319851eb378e237f07aa0f0aa22
Sanad Thapa <sanadtm@gmail.com>
2024-12-04 00:08:50 -0800

    Added more test cases for Healer Duck

--- test/Team3/HealerDuckTest.java
+++ test/Team3/HealerDuckTest.java
@@ -21,7 +21,21 @@ public class HealerDuckTest {
     @BeforeEach
     public void setUp() throws GameActionException {
         rc = mock(RobotController.class);
+        when(rc.getTeam()).thenReturn(Team.A); // Mock a valid team
+        when(rc.getTeam().opponent()).thenReturn(Team.B); // Mock the opponent team
         healerDuck = new HealerDuck(rc);
+    }
+    @Test
+    void testAttack() throws GameActionException {
+        RobotInfo enemy = mock(RobotInfo.class, withSettings().lenient());
+        MapLocation enemyLocation = new MapLocation(7, 7);
+        when(enemy.getLocation()).thenReturn(enemyLocation);
+        when(rc.senseNearbyRobots(GameConstants.VISION_RADIUS_SQUARED, rc.getTeam().opponent())).thenReturn(new RobotInfo[]{enemy});
+        when(rc.canAttack(enemyLocation)).thenReturn(true);
+        doNothing().when(rc).attack(enemyLocation);
+        boolean attackedRobots = healerDuck.attack();
+        assertEquals(false, attackedRobots);
+
     }
 
 
